7f36eab7170a064cd049e23ade66d4f51a53dc7f
Pratik Jade <pratikjade21@gmail.com>
2023-10-31 12:25:17 +0530

    update

--- Project_code/GUI/test.py
+++ Project_code/GUI/test.py
@@ -31,14 +31,74 @@ def predict(text):
     ]
     return response
 
-# Streamlit app
-st.title("AI detector")
+# Create a Streamlit app
+st.set_page_config(
+    page_title="AI Detector",
+    page_icon="✅",
+    layout="wide",
+)
 
-# Text input for prediction
-text = st.text_area("Enter some text")
+# Initialize session state
+if 'started' not in st.session_state:
+    st.session_state.started = False
 
-# Perform prediction if text is provided
-if st.button('analaize', type="primary"):
-    st.button("Reset")
-    result = predict(text)
-    st.json(result)
+# Check if the "Get Started" button is clicked
+if not st.session_state.started:
+    st.markdown("<div style='display: flex; justify-content: center; align-items: center; height: 80vh;'><h1>Welcome to AI Detector</h1></div>", unsafe_allow_html=True)
+    if st.button("Get Started"):
+        st.session_state.started = True
+
+# If the app has been started
+if st.session_state.started:
+    st.title("AI Detector")
+    
+    # Use HTML to create a heading in the main app
+    st.markdown("<h1 style='text-align:center;'>Detect AI Content</h1>", unsafe_allow_html=True)
+
+    # Add some space
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    # Create a text input with a label
+    text = st.text_area("Enter some text", height=100)
+
+    # Add space between input and button
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    # Use CSS to style the button
+    button_style = """
+        <style>
+            div.stButton > button:first-child {
+                background-color: #4CAF50;
+                color: white;
+                font-size: 18px;
+                border: none;
+                cursor: pointer;
+            }
+            div.stButton > button:first-child:hover {
+                background-color: #45a049;
+            }
+        </style>
+    """
+    st.markdown(button_style, unsafe_allow_html=True)
+
+    # Create a button to trigger the analysis
+    if st.button("Analyze"):
+        result = predict(text)
+        st.markdown("<hr>", unsafe_allow_html=True)
+        st.markdown("<h2>Analysis Result:</h2>", unsafe_allow_html=True)
+
+        if result[0]['label'] == 'AI':
+            label_color = "color: #4CAF50;"
+        else:
+            label_color = "color: #FF5733;"
+
+        # Use HTML to style the prediction result
+        result_html = f"<p style='font-size: 20px; font-weight: bold; {label_color}'>Predicted Label: {result[0]['label']}</p>"
+        score_html = f"<p style='font-size: 16px;'>Confidence Score: {result[0]['score']:.2%}</p>"
+        st.markdown(result_html, unsafe_allow_html=True)
+
+        # Add a status bar for the Confidence Score with percentages
+            # Add a status bar for the Confidence Score with percentages
+        score_html = f"<p style='font-size: 16px;'>Probability this text was entirely written by {result[0]['label']},is  {result[0]['score']}</p>"
+        st.markdown(score_html, unsafe_allow_html=True)
+        st.progress(result[0]['score'])
